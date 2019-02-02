e238806f040528b45ab98a5c7375d9d64cd02c2b
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-02 12:07:45 +0000

    Added Bank Model

---
+++ app/Models/Bank.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hover Software Soln
+ * Date: 2/2/2019
+ * Time: 9:40 AM
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bank extends Model
+{
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'banks';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'desc', 'country', 'city', 'town'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+}

9e84b2806e82466442235c2f249efae12e9b6304
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-02 12:10:35 +0000

    Added Bank Controller

---
+++ app/Http/Controllers/BankController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bank;
+use App\Search\BankSearch;
+use Illuminate\Http\Request;
+
+class BankController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function getOneOrAllBanks(Request $request, Bank $Bank)
+    {
+        $result = BankSearch::apply($request);
+        if ($result->count() == 1){
+            $result = $result->first();
+        }
+        return response()->json($result);
+    }
+
+    public function create(Request $request)
+    {
+        $author = Bank::create($request->all());
+
+        return response()->json($author, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $Bank = Bank::findOrFail($id);
+        $Bank->update($request->all());
+
+        return response()->json($Bank, 200);
+    }
+
+    public function delete($id)
+    {
+        Bank::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
+}

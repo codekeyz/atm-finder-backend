398f84fe92d5ed533eb50b31095d851930504b3a
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-02 09:40:03 +0000

    Created Migrations for ATM's

---
+++ database/migrations/2019_02_02_093617_create_atms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAtmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->double('lat');
+            $table->double('lng');
+            $table->string('bank_id');
+            $table->string('city');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atms');
+    }
+}

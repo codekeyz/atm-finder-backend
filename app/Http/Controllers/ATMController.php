77049617507de7e375ecf09342f844c0eb8a8fa3
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-02 09:46:05 +0000

    Added ATMController

--- app/Http/Controllers/ATMController.php
+++ app/Http/Controllers/ATMController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
-class ExampleController extends Controller
+use App\Models\ATM;
+use Illuminate\Http\Request;
+
+class ATMController extends Controller
 {
     /**
      * Create a new controller instance.
@@ -14,5 +17,34 @@ class ExampleController extends Controller
         //
     }
 
-    //
+    public function getAllATMs()
+    {
+        return response()->json(ATM::all());
+    }
+
+    public function getOneATM($id)
+    {
+        return response()->json(ATM::find($id));
+    }
+
+    public function create(Request $request)
+    {
+        $author = ATM::create($request->all());
+
+        return response()->json($author, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $author = ATM::findOrFail($id);
+        $author->update($request->all());
+
+        return response()->json($author, 200);
+    }
+
+    public function delete($id)
+    {
+        ATM::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
 }
--- app/Models/ATM.php
+++ app/Models/ATM.php
@@ -9,7 +9,24 @@
 namespace App\Models;
 
 
-class ATM
+use Illuminate\Database\Eloquent\Model;
+
+class ATM extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'lat', 'lng', 'bank_id', 'city'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
 
 }

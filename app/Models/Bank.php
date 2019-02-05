2a851a0e336fe9c3f2f978e46617cdc9675f31df
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-05 10:55:57 +0000

    Added bank-has-many-atms relationship

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -38,6 +38,6 @@ class Bank extends Model
     protected $hidden = [];
 
     public function atms() {
-        return $this->hasMany(ATM::class, 'bank_id');
+        return $this->hasMany(ATM::class);
     }
 }

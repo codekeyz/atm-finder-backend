1295364bfb55541b70eebc05876861444c453a81
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-05 14:44:19 +0000

    Configured Auth for Bank

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -5,17 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Bank;
 use App\Search\BankSearch;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\JWTAuth;
 
 class BankController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
+     * @var \Tymon\JWTAuth\JWTAuth
      */
-    public function __construct()
+    protected $jwt;
+
+    public function __construct(JWTAuth $jwt)
     {
-        //
+        $this->jwt = $jwt;
     }
 
     public function getOneOrAllBanks(Request $request, Bank $Bank)
@@ -46,5 +47,35 @@ class BankController extends Controller
     {
         Bank::findOrFail($id)->delete();
         return response('Deleted Successfully', 200);
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email'    => 'required|email|max:255',
+            'password' => 'required',
+        ]);
+
+        try {
+
+            if (! $token = $this->jwt->attempt($request->only('email', 'password'))) {
+                return response()->json(['user_not_found'], 404);
+            }
+
+        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], 500);
+
+        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], 500);
+
+        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
+
+            return response()->json(['token_absent' => $e->getMessage()], 500);
+
+        }
+
+        return response()->json(compact('token'));
     }
 }
--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -8,14 +8,17 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
+
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Bank extends Model implements Authenticatable
+class Bank extends Model implements JWTSubject, AuthenticatableContract, AuthorizableContract
 {
-
-    use AuthenticableTrait;
+    use Authenticatable, Authorizable;
 
     protected $fillable = ['name', 'email', 'password', 'desc', 'country', 'city', 'town'];
 
@@ -29,5 +32,15 @@ class Bank extends Model implements Authenticatable
     public function atms()
     {
         return $this->hasMany(ATM::class);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }

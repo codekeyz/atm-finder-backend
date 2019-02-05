6fa0f964992d5aefd9e325aa43116211b108a0b5
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-05 11:15:44 +0000

    Added Manager Model & Migration

---
+++ database/migrations/2019_02_05_111244_create_managers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateManagersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('managers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('bank_id')->unsigned()->index();
+            $table->foreign('bank_id')->references('id')->on('banks')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('managers');
+    }
+}

a6bf624b4c82441bb436f88232f9a31f5ca0bc10
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-07 17:50:52 +0000

    Added ATMResource

---
+++ app/Http/Resources/ATMResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\Resource;
+
+class ATMResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'status' => $this->status,
+            'coordinate' => [
+                'lat' => $this->lat,
+                'lng' => $this->lng
+            ],
+            'bank' => [
+                'name' => $this->bank->name,
+                'desc' => $this->bank->desc,
+                'country' => $this->bank->country,
+                'city' => $this->bank->city,
+                'town' => $this->bank->town
+            ],
+            'created_at' => $this->when($request->user(), $this->created_at->toDateTimeString()),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}

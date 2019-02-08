2e02611e1aa0ebd6196e8ab9cbb0e705dfbe2401
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-08 17:56:00 +0000

    Added ManagerResource

---
+++ app/Http/Resources/ManagerResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class ManagerResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->status,
+            'created_at' => $this->when($request->user(), $this->created_at->toDateTimeString()),
+            'updated_at' => $this->when($request->user(), $this->updated_at->toDateTimeString())
+        ];
+    }
+}

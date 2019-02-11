e9e82d1fdcf747a806031b443fbe440225854360
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-11 13:13:15 +0000

    Added BankResource

---
+++ app/Http/Resources/BankResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class BankResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'slogan' => $this->desc,
+            'city' => $this->city,
+            'country' => $this->country,
+            'total_atms' => $this->atms->count(),
+            'total_managers' => $this->managers->count()
+        ];
+    }
+}

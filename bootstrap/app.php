c5ef54e396d7c3fc8392e208655ee628bbfeaeb2
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-13 08:45:01 +0000

    Renamed config.php to jwt.php & refactored app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -83,6 +83,7 @@ $app->routeMiddleware([
 $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+$app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*

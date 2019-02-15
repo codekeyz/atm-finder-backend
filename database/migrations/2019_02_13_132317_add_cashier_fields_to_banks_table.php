3d6273af82365d54eb2970366c626fbd4420214d
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-15 23:32:06 +0000

    Refactored Project

--- database/migrations/2019_02_13_132317_add_cashier_fields_to_banks_table.php
+++ database/migrations/2019_02_13_132317_add_cashier_fields_to_banks_table.php
@@ -19,6 +19,18 @@ class AddCashierFieldsToBanksTable extends Migration
             $table->string('card_last_four')->nullable();
             $table->timestamp('trial_ends_at')->nullable();
         });
+
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('name');
+            $table->string('stripe_id');
+            $table->string('stripe_plan');
+            $table->integer('quantity');
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -36,5 +48,7 @@ class AddCashierFieldsToBanksTable extends Migration
                 'trial_ends_at'
             ]);
         });
+
+        Schema::dropIfExists('subscriptions');
     }
 }

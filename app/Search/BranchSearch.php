1f91c8a566d60f787e4aeb4019aced65d9a16d85
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-16 15:08:56 +0000

    Refactored BranchSearch

--- app/Search/BranchSearch.php
+++ app/Search/BranchSearch.php
@@ -10,33 +10,30 @@ namespace App\Search;
 
 
 use App\Models\ATM;
+use App\Models\Branch;
 use Illuminate\Http\Request;
 
-class ATMSearch
+class BranchSearch
 {
     public static function apply(Request $filters)
     {
-        $atm = (new ATM)->newQuery();
+        $atm = (new Branch)->newQuery();
 
         // Return atms for a bank
         if ($filters->user()) {
             $atm->where('bank_id', $filters->user()->id);
         }
 
-        // Search for an atm based on it's id.
+        // Search for an branch based on it's id.
         if ($filters->has('id')){
             $atm->where('id', $filters->get('id'));
         }
 
-        // Search for an atm based on it's name.
+        // Search for a branch based on it's name.
         if ($filters->has('name')) {
             $atm->where('name', $filters->input('name'));
         }
 
-        // Search for an atm based on it's status
-        if ($filters->has('status')) {
-            $atm->where('status', $filters->input('status'));
-        }
         return $atm->paginate(20);
     }
 }

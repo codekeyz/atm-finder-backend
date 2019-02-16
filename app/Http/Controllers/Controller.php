be0f458063458215d5f2e2468975c4cd148ac548
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-16 17:31:52 +0000

    Added sendErrorMessage function to base Controller class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,11 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public function sendErrorMessage($errorCode, $success, $errorMessage) {
+        return response()->json([
+            'success' => $success,
+            'errorMessage' => $errorMessage,
+            'errorCode' => $errorCode
+        ], $errorCode);
+    }
 }

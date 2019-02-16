1539cad57e46fec5c7775c9652030699d76cae29
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-16 00:22:32 +0000

    Added get Plans function to SubscriptionController

--- app/Http/Controllers/SubscriptionsController.php
+++ app/Http/Controllers/SubscriptionsController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Stripe\Plan;
+use Stripe\Stripe;
 
 class SubscriptionsController extends Controller
 {
-    //
+
+    public function __construct()
+    {
+        Stripe::setApiKey(env('STRIPE_SECRET'));
+    }
+
+    public function getPlans() {
+        return response()->json(['data' => Plan::all()->data]);
+    }
 }

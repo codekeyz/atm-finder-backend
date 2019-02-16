a1553b60883e13ef10549f59a51ca0817eb4fe9d
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-16 15:37:51 +0000

    Added Authenticatable Traits to Manager

--- app/Models/Manager.php
+++ app/Models/Manager.php
@@ -8,11 +8,17 @@
 
 namespace App\Models;
 
-
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Manager extends Model
+class Manager extends Model  implements JWTSubject, AuthenticatableContract, AuthorizableContract
 {
+    use Authenticatable, Authorizable;
+
     /**
      * The table associated with the model.
      *
@@ -48,5 +54,25 @@ class Manager extends Model
 
     public function branch() {
         return $this->belongsTo(Branch::class);
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }

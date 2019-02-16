c811d68930278a32a81efd31fcd3731e9e5dcf30
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-16 17:32:29 +0000

    Refactored Authenticate Middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -36,7 +36,11 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         if ($this->auth->guard($guard)->guest()) {
-            return response('Unauthorized.', 401);
+            return response()->json([
+                'success' => false,
+                'errorMessage' => 'Unauthorized Access',
+                'errorCode' => 401
+            ], 401);
         }
 
         return $next($request);
